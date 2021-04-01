5cdaca22cc32cc4c84118eca555198f419247442
Pankaj Rana <40510912+rpankaj853@users.noreply.github.com>
2021-04-01 12:54:49 +0530

    Update settings.py

--- radditcone/radditcone/settings.py
+++ radditcone/radditcone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gkbk4awp50eur#wp5d!m-y2!2(_&km)2_(b=a)n_x5rva#o-iw'
+SECRET_KEY = 'you have to use your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
